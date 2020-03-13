29283b0b0f211e8e979011fbc60b9c26e846a369
Evgeny <ek@idem.agency>
2020-03-13 18:14:34 +0400

    gem

--- webpack-master/source/entity/index/index.js
+++ webpack-master/source/entity/index/index.js
@@ -1,1 +1,8 @@
+import objectFitImages from 'object-fit-images';
 require('./index.scss');
+
+const images = document.querySelectorAll('.picture > img');
+
+images.forEach((item) => {
+  objectFitImages(item);
+});
